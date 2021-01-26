5fb3cf563c55c3c0d8c2d2535f19f795373509f1
Sebastian Franz <sebastian.franz@online.de>
2021-01-26 10:44:12 +0100

    Added eslint

---
+++ src/pages/index.test.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from './index';
+
+describe('App', () => {
+  it('renders without crashing', () => {
+    render(<App />);
+    expect(
+      screen.getByRole('heading', { name: 'Welcome to Next.js!' }),
+    ).toBeInTheDocument();
+  });
+});
